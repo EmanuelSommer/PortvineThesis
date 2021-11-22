c4d066316754c47c67160d97d0f12a9071d9d266
EmanuelSommer <emanuel.sommer@web.de>
2021-11-23 00:01:36 +0100

    dnorm vs dt viz

--- theoretical_viz.R
+++ theoretical_viz.R
@@ -2,6 +2,37 @@
 
 library(tidyverse)
 library(latex2exp)
+# colors
+my_blue <- "#92B8DE"
+my_red <- "#db4f59"
+
+
+### Normal vs Student's t --------------------------------------------
+ggplot() +
+  stat_function(fun = dnorm, size = 1, n = 100001,
+                aes(col = "norm", linetype = "norm")) +
+  stat_function(fun = dt, size = 1, n = 100001, args = list(df = 2),
+                aes(col = "t", linetype = "t")) +
+  scale_color_manual(values = c("norm" = "black", "t" = my_blue),
+                     labels = c("N(0,1)", expression(t[2](0,1))),
+                     name = "") +
+  scale_linetype_manual(values = c("norm" = "solid", "t" = "twodash"),
+                        labels = c("N(0,1)", expression(t[2](0,1))),
+                        name = "") +
+  scale_x_continuous(limits = c(-5, 5), breaks = seq(-6, 6, 2)) +
+  labs(y = "Density", x = TeX(""),
+       title = TeX("Densities of the standard normal and Student's t distribution")) +
+  theme_light() +
+  theme(panel.grid.minor.y = element_blank(),
+        panel.grid.minor.x = element_blank(),
+        panel.border = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major = element_line(colour = "grey", size = .2),
+        axis.text = element_text(size = 10),
+        axis.title = element_text(size = 12),
+        legend.position = c(0.77, 0.66),
+        legend.background = element_blank(),
+        legend.text = element_text(size = 12))
 
 ### Value at Risk ----------------------------------------------------
 ggplot() +
